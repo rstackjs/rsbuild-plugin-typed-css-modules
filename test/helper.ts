c01a37bfcae718dddcc05844a30a164bbadebf25
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-07-13 14:22:37 +0800

    chore: reuse shared test utilities (#75)

--- test/helper.ts
+++ test/helper.ts
@@ -1,21 +1,6 @@
 import fs from 'node:fs';
 import { join } from 'node:path';
 
-const portMap = new Map();
-
-export function getRandomPort(
-  defaultPort = Math.ceil(Math.random() * 30000) + 15000,
-) {
-  let port = defaultPort;
-  while (true) {
-    if (!portMap.get(port)) {
-      portMap.set(port, 1);
-      return port;
-    }
-    port++;
-  }
-}
-
 export const generatorTempDir = async (cwd: string, testDir: string) => {
   fs.rmSync(testDir, { recursive: true, force: true });
   await fs.promises.cp(join(cwd, 'src'), testDir, { recursive: true });
